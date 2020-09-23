612e67d43c5437ce34ba981b842885ee00d16221
DesmondWasHere <subratkatiyar50@gmail.com>
2020-09-23 10:05:43 +0530

    added text encrypt decrypt functions.

--- secure_input_output.py
+++ secure_input_output.py
@@ -1,14 +1,14 @@
 from encryption.aes_implementation import *
 # import string
-def sinput(prompt = "", passcode = ""):
+def sinput(prompt = "", passcode = "*"):
     secure_variable = encrypt(input(prompt), passcode)
     return (secure_variable)
 
-def sprint(secure_variable, passcode = ""):
+def sprint(secure_variable, passcode = "*"):
     deafult_string = decrypt(secure_variable,passcode)
     return (deafult_string)
 
-# Use case Examplesn
+# Use case Examples
 a = sinput("Enter input: ","password")
 print(type(a))
 print("Encrypted Text is: ",a)
