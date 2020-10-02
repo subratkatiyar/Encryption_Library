7e8d7de9ec3f2e60467fd67293477712c3de758d
Gagan <gaganyadav80@gmail.com>
2020-10-03 00:06:37 +0530

    Firebase user Login/Signup and Create/Join chatroom

--- secure_input_output.py
+++ secure_input_output.py
@@ -1,15 +1,19 @@
 from encryption.aes_implementation import *
 # import string
-def sinput(prompt = "", passcode = "*"):
-    secure_variable = encrypt(input(prompt), passcode)
+
+
+def sinput(key='', passcode="*"):
+    secure_variable = encrypt(key, passcode)
     return (secure_variable)
 
-def sprint(secure_variable, passcode = "*"):
-    deafult_string = decrypt(secure_variable,passcode)
+
+def sprint(secure_variable, passcode="*"):
+    deafult_string = decrypt(secure_variable, passcode)
     return (deafult_string)
 
+
 # Use case Examples
-a = sinput("Enter input: ","password")
-print(type(a))
-print("Encrypted Text is: ",a)
-print("Decrypted Text is: ",sprint(a,"password"))
+# a = sinput("Enter input: ", "password")
+# print(type(a))
+# print("Encrypted Text is: ", a)
+# print("Decrypted Text is: ", sprint(a, "password"))
